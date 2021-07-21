495b3cf844df1f1a512f5c19f4f76a01fba061f3
sandrosolomon1 <74678768+sandrosolomon1@users.noreply.github.com>
2021-07-21 16:34:06 +0400

    Update CustomerCompanyAttribute.php

--- Setup/Patch/Data/CustomerCompanyAttribute.php
+++ Setup/Patch/Data/CustomerCompanyAttribute.php
@@ -1,114 +1,72 @@
 <?php
-
 namespace Devall\Company\Setup\Patch\Data;
 
-use Magento\Customer\Api\CustomerMetadataInterface;
-use Magento\Eav\Api\AttributeRepositoryInterface;
-use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 use Magento\Framework\Setup\Patch\PatchRevertableInterface;
-use Magento\Eav\Setup\EavSetupFactory;
-use Magento\Eav\Model\Config;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Customer\Setup\CustomerSetup;
 
-class CustomerCompanyAttribute
-    implements DataPatchInterface,
-    PatchRevertableInterface
+class CustomerCompanyAttribute implements DataPatchInterface, PatchRevertableInterface
 {
-
-    /**
-     * @var EavSetupFactory
-     */
-    private $eavSetupFactory;
 
     /**
      * @var ModuleDataSetupInterface
      */
     private $moduleDataSetup;
+    /**
+     * @var CustomerSetup
+     */
+    private $customerSetupFactory;
 
     /**
-     * @var AttributeRepositoryInterface
-     */
-    private $attributeRepository;
-    /**
-     * @var Config
-     */
-    private $eavConfig;
-
-    /**
+     * Constructor
+     *
      * @param ModuleDataSetupInterface $moduleDataSetup
-     * @param EavSetupFactory $eavSetupFactory
-     * @param Config $eavConfig
-     * @param AttributeRepositoryInterface $attributeRepository
+     * @param CustomerSetupFactory $customerSetupFactory
      */
     public function __construct(
         ModuleDataSetupInterface $moduleDataSetup,
-        EavSetupFactory $eavSetupFactory,
-        Config $eavConfig,
-        AttributeRepositoryInterface $attributeRepository
+        CustomerSetupFactory $customerSetupFactory
     ) {
         $this->moduleDataSetup = $moduleDataSetup;
-        $this->eavSetupFactory = $eavSetupFactory;
-        $this->eavConfig       = $eavConfig;
-        $this->attributeRepository = $attributeRepository;
+        $this->customerSetupFactory = $customerSetupFactory;
     }
 
     /**
      * {@inheritdoc}
-     * @throws \Exception
      */
-    public function apply()
-    {
+    public function apply() {
         $this->moduleDataSetup->getConnection()->startSetup();
-
-        $attr = "company_attr";
-
-        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
-        $eavSetup->addAttribute(
-            CustomerMetadataInterface::ENTITY_TYPE_CUSTOMER,
-            $attr,
+        /** @var CustomerSetup $customerSetup */
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $customerSetup->addAttribute(
+            \Magento\Customer\Model\Customer::ENTITY,
+            'company',
             [
-                'type'         => 'int',
-                'label'        => 'Company attr',
-                'input'        => 'text',
-                'required'     => false,
-                'visible'      => true,
-                'user_defined' => true,
-                'position'     => 999,
-                'system'       => 0,
+                'type' => 'int',
+                'label' => 'Company',
+                'input' => 'text',
+                'source' => '',
+                'required' => false,
+                'visible' => true,
+                'position' => 500,
+                'system' => false,
+                'backend' => ''
             ]
         );
-        $eavSetup->addAttributeToSet(
-            CustomerMetadataInterface::ENTITY_TYPE_CUSTOMER,
-            CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER,
-            null,
-            'company_attr');
 
-        $companyAttribute = $this->eavConfig->getAttribute(CustomerMetadataInterface::ENTITY_TYPE_CUSTOMER, $attr)
-            ->setData('used_in_forms', [
-                'adminhtml_customer',
-                'customer_account_create',
-                'customer_account_edit'
-            ]);
-
-        $this->attributeRepository->save($companyAttribute);
+        $attribute = $customerSetup->getEavConfig()->getAttribute('customer', 'company')->addData(['used_in_forms' => ['adminhtml_customer']]);
+        $attribute->save();
 
         $this->moduleDataSetup->getConnection()->endSetup();
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function getDependencies(): array
-    {
-        return [];
-    }
-
-    public function revert()
-    {
+    public function revert() {
         $this->moduleDataSetup->getConnection()->startSetup();
-
-        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
-        $eavSetup->removeAttribute(\Magento\Customer\Model\Customer::ENTITY, 'company_attr');
+        /** @var CustomerSetup $customerSetup */
+        $customerSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $customerSetup->removeAttribute(\Magento\Customer\Model\Customer::ENTITY, 'company');
 
         $this->moduleDataSetup->getConnection()->endSetup();
     }
@@ -116,8 +74,16 @@ class CustomerCompanyAttribute
     /**
      * {@inheritdoc}
      */
-    public function getAliases(): array
-    {
+    public function getAliases() {
         return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies() {
+        return [
+
+        ];
     }
 }
