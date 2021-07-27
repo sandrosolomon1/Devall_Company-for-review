c815d19b35bd328d8805ce461310365d2d613fb3
sandrosolomon1 <74678768+sandrosolomon1@users.noreply.github.com>
2021-07-27 07:05:52 +0400

    Update company.js

--- view/frontend/web/js/company.js
+++ view/frontend/web/js/company.js
@@ -1,46 +1,56 @@
-define([
+define(
+    [
         'uiComponent',
         'ko',
         'jquery'
     ],
-    function(Component, ko, $) {
+    function (Component, ko, $) {
         'use strict';
-        return Component.extend({
-            country: ko.observable(''),
-            street_number: ko.observable(''),
-            street: ko.observable(''),
-            size: ko.observable(''),
+        return Component.extend(
+            {
+                country: ko.observable(''),
+                streetNumber: ko.observable(''),
+                street: ko.observable(''),
+                size: ko.observable(''),
 
-            initialize: function(config) {
-                this._super();
-                this.country = ko.observable(config.country);
-                this.street = ko.observable(config.street);
-                this.street_number = ko.observable(config.street_number);
-                this.size = ko.observable(config.size);
-                var root = this;
+                initialize: function (config) {
+                    this._super();
+                    this.country = ko.observable(config.country);
+                    this.street = ko.observable(config.street);
+                    this.streetNumber = ko.observable(config.streetNumber);
+                    this.size = ko.observable(config.size);
+                    var root = this;
 
-                ourAsyncFunction($('#company').val());
+                    ourAsyncFunction($('#company').val());
 
-                $('#company').on('change', function() {
-                    ourAsyncFunction($(this).val());
-                })
-
-                async function ourAsyncFunction(id) {
-                    $.ajax({
-                        url: `${config.rest_api_url}/${id}`,
-                        success: function(res){
-                            setCompanyDetails(res);
+                    $('#company').on(
+                        'change',
+                        function () {
+                            ourAsyncFunction($(this).val());
                         }
-                    })
-                }
+                    )
 
-                function setCompanyDetails(data) {
-                    root.country(data.country);
-                    root.street(data.street);
-                    root.street_number(data.number);
-                    root.size(data.size);
+                    function ourAsyncFunction(id)
+                    {
+                        $.ajax(
+                            {
+                                url: `${config.restApiUrl}/${id}`,
+                                success: function (res) {
+                                    setCompanyDetails(res);
+                                }
+                            }
+                        )
+                    }
+
+                    function setCompanyDetails(data)
+                    {
+                        root.country(data.country);
+                        root.street(data.street);
+                        root.streetNumber(data.number);
+                        root.size(data.size);
+                    }
                 }
             }
-        });
+        );
     }
 );
