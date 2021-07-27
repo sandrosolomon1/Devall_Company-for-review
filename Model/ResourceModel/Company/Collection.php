a8ce9cbd4845302b41f8f6ecafc757f4c4b27e28
sandrosolomon1 <74678768+sandrosolomon1@users.noreply.github.com>
2021-07-27 06:56:41 +0400

    Update Collection.php

--- Model/ResourceModel/Company/Collection.php
+++ Model/ResourceModel/Company/Collection.php
@@ -1,9 +1,13 @@
 <?php
-
+declare(strict_types=1);
 namespace Devall\Company\Model\ResourceModel\Company;
 
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 
+/**
+ * Class Collection
+ * @package Devall\Company\Model\ResourceModel\Company
+ */
 class Collection extends AbstractCollection
 {
     protected function _construct()
